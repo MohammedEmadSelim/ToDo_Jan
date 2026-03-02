9e9a6217c9b49068b5d96b81e9a07e4bcc5ae7be
Mohammed <mohammedemad557@yahoo.com>
2026-03-02 15:50:42 +0200

    sign out

--- lib/features/home_screen/presentation/components/todo_card.dart
+++ lib/features/home_screen/presentation/components/todo_card.dart
@@ -1,15 +1,10 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:todo_app/core/responsive/responive_extention.dart';
 import 'package:todo_app/core/theme/app_colors.dart';
 import 'package:todo_app/features/home_screen/data/models/todo_model.dart';
 
 class TodoCart extends StatelessWidget {
-  const TodoCart({
-    super.key,
-    required this.todo, this.onTap,
-  });
+  const TodoCart({super.key, required this.todo, this.onTap});
 
   final TodoModel todo;
   final void Function()? onTap;
@@ -26,9 +21,9 @@ class TodoCart extends StatelessWidget {
         ),
         child: Column(
           children: [
-            SizedBox(height: 10.h,),
+            SizedBox(height: 10.h),
             Padding(
-              padding:  EdgeInsets.symmetric(horizontal: 14.w),
+              padding: EdgeInsets.symmetric(horizontal: 14.w),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -45,25 +40,26 @@ class TodoCart extends StatelessWidget {
               ),
             ),
             SizedBox(height: 24.h),
-            Expanded(
+            Flexible(
               child: Text(
                 todo.des,
-                maxLines: 4,
+                maxLines: 3,
                 style: TextStyle(
-
                   color: AppColors.textWhite,
                   fontWeight: FontWeight.w500,
                   fontSize: 18.sp,
-                  overflow: TextOverflow.ellipsis
+                  overflow: TextOverflow.ellipsis,
                 ),
               ),
             ),
-
+            SizedBox(height: 10.h),
+            Spacer(),
             Padding(
-              padding:  EdgeInsets.symmetric(horizontal: 21.w,vertical: 10.h),
+              padding: EdgeInsets.symmetric(horizontal: 21.w, vertical: 10.h),
               child: Row(
                 children: [
-                  Text(todo.deadline,
+                  Text(
+                    todo.deadline,
                     style: TextStyle(
                       color: AppColors.textWhite,
                       fontWeight: FontWeight.w500,
